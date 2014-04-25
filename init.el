12d745726a33e780c74ef23d5c663b8ffe6685ee
Ganesh N. Sivalingam <g.n.sivalingam@gmail.com>
2014-04-25 09:20:42 +0100

    Setup using gmail through emacs using gnus

--- init.el
+++ init.el
@@ -22,7 +22,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files (quote ("~/Dropbox/workspaces/Challenger/Challenger.org"))))
+ '(org-agenda-files (quote ("~/Dropbox/workspaces/Challenger/Challenger.org")))
+ '(smtpmail-smtp-server "smtp.gmail.com")
+ '(smtpmail-smtp-service 25))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
