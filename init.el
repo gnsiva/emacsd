f945f228d0388c2e1a246f29275afcb1fe56befc
Ganesh N Sivalingam <g.n.sivalingam@gmail.com>
2014-05-04 10:27:49 +0100

    Changed emacs theme and started playing around with M-x customize

--- init.el
+++ init.el
@@ -22,9 +22,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (wombat)))
  '(org-agenda-files (quote ("~/Dropbox/workspaces/Challenger/Challenger.org")))
  '(smtpmail-smtp-server "smtp.gmail.com")
- '(smtpmail-smtp-service 25))
+ '(smtpmail-smtp-service 25)
+ '(uniquify-buffer-name-style (quote post-forward) nil (uniquify)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
